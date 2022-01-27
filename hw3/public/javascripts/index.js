e5e4195157526710aab0e6e05477de3074a17845
ching23 <74621195+ching23@users.noreply.github.com>
2022-01-26 22:32:06 -0800

    moved files

---
+++ hw3/public/javascripts/index.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $(".orderButton").click(function(){
+        // variables for radio buttons, textbox, and drop down
+        let toppings = $('input[class="toppings"]:checked').val();
+        let quantity = $("#quantity option:selected").val();
+        let instructions = $("#textOrder").val()
+
+        // if textarea includes the word vegan, display warning message
+        if(instructions.includes("vegan")) {
+            alert("WARNING: Cheesecakes Contain Dairy");
+            // return false, so that the page doesn't reload
+            return false;
+        }
+        else {
+            $("#cheesecakeForm").remove();
+            
+            $("#parNotes").text("Thank you! Your order has been placed.");
+            $("#orderInfo").text("Quantity: " + quantity + ", toppings: " + toppings + ", notes: " + instructions);
+        }
+    });
+    // make hover button clickable and change text that is selected
+    $(".month-link").click(function(){
+        let month = $(this).text();
+        $(".monthDrop").text(month);
+    });
+});
